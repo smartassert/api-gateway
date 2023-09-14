899182bf0e3206b22b9fa2133416dc66290e19e6
Jon Cram <webignition@gmail.com>
2023-09-14 10:09:58 +0100

    Remove handling of /user/api/token/verify (#64)

--- src/Controller/UserApiTokenController.php
+++ src/Controller/UserApiTokenController.php
@@ -9,7 +9,6 @@ use App\Response\ErrorResponseBody;
 use App\Response\LabelledBody;
 use App\Response\Response;
 use App\Response\User\Token;
-use App\Response\User\User;
 use App\Security\AuthenticationToken;
 use Psr\Http\Client\ClientExceptionInterface;
 use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
@@ -18,7 +17,6 @@ use SmartAssert\ServiceClient\Exception\InvalidResponseTypeException;
 use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
 use SmartAssert\UsersClient\Client;
 use SmartAssert\UsersClient\Exception\UnauthorizedException;
-use SmartAssert\UsersClient\Model\Token as UsersClientToken;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -114,95 +112,6 @@ readonly class UserApiTokenController
             new LabelledBody(
                 'token',
                 new Token($token->token)
-            )
-        );
-    }
-
-    #[Route('/verify', name: 'verify', methods: ['GET'])]
-    public function verify(AuthenticationToken $token): JsonResponse
-    {
-        try {
-            $user = $this->client->verifyApiToken(new UsersClientToken($token->token));
-            if (null === $user) {
-                return new ErrorResponse(new ErrorResponseBody('unauthorized'), 401);
-            }
-        } catch (ClientExceptionInterface $e) {
-            $code = $e->getCode();
-            $message = $e->getMessage();
-
-            return new ErrorResponse(
-                new ErrorResponseBody(
-                    'service-communication-failure',
-                    [
-                        'service' => 'users',
-                        'error' => [
-                            'code' => $code,
-                            'message' => $message,
-                        ],
-                    ]
-                )
-            );
-        } catch (InvalidModelDataException $e) {
-            return new ErrorResponse(
-                new ErrorResponseBody(
-                    'invalid-model-data',
-                    [
-                        'service' => 'users',
-                        'data' => $e->getResponse()->getBody(),
-                    ]
-                )
-            );
-        } catch (InvalidResponseDataException $e) {
-            return new ErrorResponse(
-                new ErrorResponseBody(
-                    'invalid-response-data',
-                    [
-                        'service' => 'users',
-                        'data' => $e->getResponse()->getBody(),
-                        'data-type' => [
-                            'expected' => $e->expected,
-                            'actual' => $e->actual,
-                        ],
-                    ]
-                )
-            );
-        } catch (InvalidResponseTypeException $e) {
-            return new ErrorResponse(
-                new ErrorResponseBody(
-                    'invalid-response-type',
-                    [
-                        'service' => 'users',
-                        'content-type' => [
-                            'expected' => $e->expected,
-                            'actual' => $e->actual,
-                        ],
-                    ]
-                )
-            );
-        } catch (NonSuccessResponseException $e) {
-            if (404 === $e->getStatusCode()) {
-                return new ErrorResponse(
-                    new ErrorResponseBody('not-found'),
-                    $e->getStatusCode()
-                );
-            }
-
-            return new ErrorResponse(
-                new ErrorResponseBody(
-                    'non-successful-service-response',
-                    [
-                        'service' => 'users',
-                        'status' => $e->getStatusCode(),
-                        'message' => $e->getMessage(),
-                    ]
-                )
-            );
-        }
-
-        return new Response(
-            new LabelledBody(
-                'user',
-                new User($user->id, $user->userIdentifier)
             )
         );
     }
