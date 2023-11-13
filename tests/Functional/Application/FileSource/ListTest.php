5e6d7a33263d6701dc4dd728ef65cd2bd6d33656
Jon Cram <webignition@gmail.com>
2023-11-13 17:25:55 +0000

    Proxy requests for /file-source/{sourceId}/list (#125)

---
+++ tests/Functional/Application/FileSource/ListTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Application\FileSource;
+
+use App\Tests\Application\FileSource\AbstractListTest;
+use App\Tests\Functional\GetClientAdapterTrait;
+
+class ListTest extends AbstractListTest
+{
+    use GetClientAdapterTrait;
+}
---
+++ tests/Integration/FileSource/ListTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\FileSource;
+
+use App\Tests\Application\FileSource\AbstractListTest;
+use App\Tests\Integration\GetClientAdapterTrait;
+
+class ListTest extends AbstractListTest
+{
+    use GetClientAdapterTrait;
+}
