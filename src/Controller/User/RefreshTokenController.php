e96e90c3fab520707065beb1adc9f9899eceda3a
Jon Cram <webignition@gmail.com>
2023-11-14 16:10:28 +0000

    RefreshTokenController actions return empty responses

--- src/Controller/User/RefreshTokenController.php
+++ src/Controller/User/RefreshTokenController.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 namespace App\Controller\User;
 
 use App\Exception\ServiceException;
-use App\Response\EmptyBody;
-use App\Response\Response;
+use App\Response\EmptyResponse;
 use App\Security\AuthenticationToken;
 use App\Security\RefreshToken;
 use App\Security\UserId;
@@ -14,7 +13,6 @@ use Psr\Http\Client\ClientExceptionInterface;
 use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
 use SmartAssert\ServiceClient\Exception\UnauthorizedException;
 use SmartAssert\UsersClient\ClientInterface as UsersClient;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 readonly class RefreshTokenController
@@ -29,7 +27,7 @@ readonly class RefreshTokenController
      * @throws ServiceException
      */
     #[Route('/user/refresh_token/revoke-all', name: 'user_revoke_all_refresh_token', methods: ['POST'])]
-    public function revokeAllForUser(AuthenticationToken $token, UserId $userId): JsonResponse
+    public function revokeAllForUser(AuthenticationToken $token, UserId $userId): EmptyResponse
     {
         try {
             $this->client->revokeFrontendRefreshTokensForUser($token->token, $userId->id);
@@ -37,9 +35,7 @@ readonly class RefreshTokenController
             throw new ServiceException('users', $e);
         }
 
-        return new Response(
-            new EmptyBody()
-        );
+        return new EmptyResponse();
     }
 
     /**
@@ -47,7 +43,7 @@ readonly class RefreshTokenController
      * @throws ServiceException
      */
     #[Route('/user/refresh_token/revoke', name: 'user_revoke_refresh_token', methods: ['POST'])]
-    public function revoke(AuthenticationToken $token, RefreshToken $refreshToken): JsonResponse
+    public function revoke(AuthenticationToken $token, RefreshToken $refreshToken): EmptyResponse
     {
         try {
             $this->client->revokeFrontendRefreshToken($token->token, $refreshToken->refreshToken);
@@ -55,8 +51,6 @@ readonly class RefreshTokenController
             throw new ServiceException('users', $e);
         }
 
-        return new Response(
-            new EmptyBody()
-        );
+        return new EmptyResponse();
     }
 }
