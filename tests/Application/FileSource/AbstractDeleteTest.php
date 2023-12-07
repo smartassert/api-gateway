6921b35219cdb4ca2a3c5fc2cd7790adf73765f4
Jon Cram <webignition@gmail.com>
2023-12-07 18:06:17 +0000

    Apply to FileSourceController::read()

--- tests/Application/FileSource/AbstractDeleteTest.php
+++ tests/Application/FileSource/AbstractDeleteTest.php
@@ -8,6 +8,7 @@ use App\Tests\Application\AbstractApplicationTestCase;
 use App\Tests\Application\CreateSourceTrait;
 use App\Tests\Application\UnauthorizedUserDataProviderTrait;
 use SmartAssert\TestAuthenticationProviderBundle\ApiKeyProvider;
+use SmartAssert\TestAuthenticationProviderBundle\UserProvider;
 use Symfony\Component\Uid\Ulid;
 
 abstract class AbstractDeleteTest extends AbstractApplicationTestCase
@@ -43,11 +44,15 @@ abstract class AbstractDeleteTest extends AbstractApplicationTestCase
         \assert($apiKeyProvider instanceof ApiKeyProvider);
         $apiKey = $apiKeyProvider->get('user@example.com');
 
+        $userProvider = self::getContainer()->get(UserProvider::class);
+        \assert($userProvider instanceof UserProvider);
+        $user = $userProvider->get('user@example.com');
+
         $label = md5((string) rand());
         $id = $this->createFileSource($apiKey->key, $label);
 
         $getResponse = $this->applicationClient->makeReadFileSourceRequest($apiKey->key, $id);
-        $this->assertRetrievedFileSource($getResponse, $label, null, $id);
+        $this->assertRetrievedFileSource($getResponse, $label, $user->id, $id);
 
         $deleteResponse = $this->applicationClient->makeDeleteFileSourceRequest($apiKey->key, $id);
         $this->assertDeletedFileSource($deleteResponse, $label, $id);
