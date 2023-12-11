10afef18447aff7b8164268ce7ecd5c6431b2528
Jon Cram <webignition@gmail.com>
2023-12-11 17:36:28 +0000

    Apply to SuiteController::list()

--- src/Controller/Suite/SuiteController.php
+++ src/Controller/Suite/SuiteController.php
@@ -9,14 +9,6 @@ use App\Security\ApiToken;
 use App\ServiceProxy\ServiceProxy;
 use App\ServiceRequest\RequestBuilderFactory;
 use Psr\Http\Client\ClientExceptionInterface;
-use SmartAssert\ServiceClient\Exception\HttpResponseExceptionInterface;
-use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
-use SmartAssert\ServiceClient\Exception\InvalidResponseDataException;
-use SmartAssert\ServiceClient\Exception\InvalidResponseTypeException;
-use SmartAssert\ServiceClient\Exception\UnauthorizedException;
-use SmartAssert\ServiceClient\SerializableInterface;
-use SmartAssert\SourcesClient\SuiteClientInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,7 +17,6 @@ use Symfony\Component\Routing\Annotation\Route;
 readonly class SuiteController
 {
     public function __construct(
-        private SuiteClientInterface $client,
         private RequestBuilderFactory $requestBuilderFactory,
         private ServiceProxy $sourcesProxy,
     ) {
@@ -60,30 +51,20 @@ readonly class SuiteController
 
     /**
      * @throws ServiceException
-     * @throws UnauthorizedException
      */
     #[Route(path: 's', name: 'list', methods: ['GET'])]
-    public function list(ApiToken $token): Response
+    public function list(ApiToken $token, Request $request): Response
     {
+        $requestBuilder = $this->requestBuilderFactory->create($request->getMethod(), $request->getRequestUri());
+        $httpRequest = $requestBuilder
+            ->withAuthorization($token->token)
+            ->get()
+        ;
+
         try {
-            $suites = $this->client->list($token->token);
-        } catch (
-            ClientExceptionInterface |
-            HttpResponseExceptionInterface |
-            InvalidModelDataException |
-            InvalidResponseDataException |
-            InvalidResponseTypeException $e
-        ) {
-            throw new ServiceException('sources', $e);
+            return $this->sourcesProxy->sendRequest($httpRequest);
+        } catch (ClientExceptionInterface $exception) {
+            throw new ServiceException('sources', $exception);
         }
-
-        $serializedSuites = [];
-        foreach ($suites as $suite) {
-            if ($suite instanceof SerializableInterface) {
-                $serializedSuites[] = $suite->toArray();
-            }
-        }
-
-        return new JsonResponse($serializedSuites);
     }
 }
