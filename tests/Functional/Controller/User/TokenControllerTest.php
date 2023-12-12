1eac4bf18bb4e75170cf18abb683063b66024009
Jon Cram <webignition@gmail.com>
2023-12-12 17:58:13 +0000

    Apply to TokenController::create()

--- tests/Functional/Controller/User/TokenControllerTest.php
+++ tests/Functional/Controller/User/TokenControllerTest.php
@@ -6,9 +6,14 @@ namespace App\Tests\Functional\Controller\User;
 
 use App\Tests\Application\AbstractApplicationTestCase;
 use App\Tests\DataProvider\InvalidResponseModelDataProviderCreatorTrait;
+use App\Tests\DataProvider\ServiceBadResponseContentTypeDataProviderTrait;
 use App\Tests\DataProvider\ServiceHttpFailureDataProviderCreatorTrait;
+use App\Tests\DataProvider\ServiceHttpFailureDataProviderTrait;
 use App\Tests\Functional\Controller\AssertJsonResponseTrait;
 use App\Tests\Functional\GetClientAdapterTrait;
+use GuzzleHttp\Handler\MockHandler;
+use Psr\Http\Client\ClientInterface as HttpClientInterface;
+use Psr\Http\Message\ResponseInterface;
 use SmartAssert\UsersClient\ClientInterface as UsersClient;
 
 class TokenControllerTest extends AbstractApplicationTestCase
@@ -16,29 +21,32 @@ class TokenControllerTest extends AbstractApplicationTestCase
     use GetClientAdapterTrait;
     use ServiceHttpFailureDataProviderCreatorTrait;
     use InvalidResponseModelDataProviderCreatorTrait;
+    use ServiceBadResponseContentTypeDataProviderTrait;
+    use ServiceHttpFailureDataProviderTrait;
     use AssertJsonResponseTrait;
 
     /**
-     * @dataProvider usersClientExceptionDataProvider
+     * @dataProvider serviceExceptionDataProvider
      *
      * @param array<mixed> $expectedData
      */
     public function testCreateHandlesException(
-        \Exception $exception,
+        \Exception|ResponseInterface $httpFixture,
         int $expectedStatusCode,
         array $expectedData
     ): void {
+        $mockingHttpClient = self::getContainer()->get('app.test.mocking_http_client');
+        \assert($mockingHttpClient instanceof HttpClientInterface);
+
+        $httpMockHandler = self::getContainer()->get(MockHandler::class);
+        \assert($httpMockHandler instanceof MockHandler);
+
+        $httpMockHandler->append($httpFixture);
+
         $userIdentifier = md5((string) rand());
         $password = md5((string) rand());
 
-        $usersClient = \Mockery::mock(UsersClient::class);
-        $usersClient
-            ->shouldReceive('createFrontendToken')
-            ->with($userIdentifier, $password)
-            ->andThrow($exception)
-        ;
-
-        self::getContainer()->set(UsersClient::class, $usersClient);
+        self::getContainer()->set(HttpClientInterface::class, $mockingHttpClient);
 
         $response = $this->applicationClient->makeCreateUserTokenRequest($userIdentifier, $password);
 
@@ -105,6 +113,17 @@ class TokenControllerTest extends AbstractApplicationTestCase
         return array_merge(
             $this->serviceHttpFailureDataProviderCreator('users'),
             $this->invalidResponseModelDataProviderCreator('users'),
+        );
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public function serviceExceptionDataProvider(): array
+    {
+        return array_merge(
+            $this->serviceBadResponseContentTypeDataProvider('users', 'application/json'),
+            $this->serviceHttpFailureDataProvider('users'),
         );
     }
 }
