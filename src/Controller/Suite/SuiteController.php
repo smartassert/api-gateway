e75439cbc3eac18070fdf2f8c837f37e192e656f
Jon Cram <webignition@gmail.com>
2023-12-14 15:09:31 +0000

    Combine SuiteController methods

--- src/Controller/Suite/SuiteController.php
+++ src/Controller/Suite/SuiteController.php
@@ -11,7 +11,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(path: '/{serviceName<[a-z]+>}/suite', name: 'suite_')]
 readonly class SuiteController
 {
     public function __construct(
@@ -22,17 +21,8 @@ readonly class SuiteController
     /**
      * @throws ServiceException
      */
-    #[Route(path: '/{suiteId<[A-Z90-9]{26}>?}', name: 'act', methods: ['POST', 'GET', 'PUT', 'DELETE'])]
+    #[Route(path: '/{serviceName<[a-z]+>}{action<.*>}', name: 'suite_act')]
     public function act(Service $service, Request $request): Response
-    {
-        return $this->serviceProxy->proxy($service, $request);
-    }
-
-    /**
-     * @throws ServiceException
-     */
-    #[Route(path: 's', name: 'list', methods: ['GET'])]
-    public function list(Service $service, Request $request): Response
     {
         return $this->serviceProxy->proxy($service, $request);
     }
