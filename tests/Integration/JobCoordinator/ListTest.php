e67b7e9b3fb11b910cd5f511542e57fcfed6aaee
Jon Cram <webignition@gmail.com>
2024-03-27 11:09:32 +0000

    List job-coordinator jobs by suite

---
+++ tests/Integration/JobCoordinator/ListTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Integration\JobCoordinator;
+
+use App\Tests\Application\JobCoordinator\AbstractListTest;
+use App\Tests\Integration\GetClientAdapterTrait;
+
+class ListTest extends AbstractListTest
+{
+    use GetClientAdapterTrait;
+}
